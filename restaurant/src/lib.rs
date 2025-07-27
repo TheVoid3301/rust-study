c88c8bd6e1cecc22249ee6f2488407ef754bf339
thevoid3301 <1449812768@qq.com>
2025-07-28 01:17:07 +0800

    rust学习第七章crate

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,65 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // 在夏天点一份黑麦面包作为早餐
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // 更改我们想要的面包
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // 如果取消下一行的注释，将会导致编译失败；我们不被允许
+    // 看到或更改随餐搭配的季节水果
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+//当加入pub时,enum的成员也会变成公有的,但是struct的成员仍然是私有的
+pub enum Appetizer {
+    Soup,
+    Salad,
+}
